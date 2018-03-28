12a384d9d8a518e10e5ffa292094c4b8cc9fb2ae
Daniel Griffin <dgriffin5@pagesubmit.chtc.wisc.edu>
2018-03-28 11:07:09 -0500

    Updated run_inference.sh to include .zip file, and updated .sub file for gpu specs.

--- run_inference.sh
+++ run_inference.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Run the demo CNN image labeling.
+mkdir test_out
 python2 tools/infer_simple.py \
     --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
-    --output-dir /tmp/detectron-visualizations \
+    --output-dir ./test_out/ \
     --image-ext jpg \
     --wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl \
     demo
+
+# Zip the output files in output.zip
+zip -r out.zip test_out/
+
+
+
