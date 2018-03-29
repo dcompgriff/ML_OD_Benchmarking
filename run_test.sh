d377fad763f0802e92b34f3dd7c5b1e3c83e1713
Daniel Griffin <dgriffin5@pagesubmit.chtc.wisc.edu>
2018-03-29 18:00:03 -0500

    Updated run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -2,8 +2,8 @@
 
 # Run the demo CNN image labeling.
 mkdir /tmp/test_out
-
-python hello_condor_docker.py
+pwd
+python /ML_OD_Benchmarking/hello_condor_docker.py
 
 # Zip the output files in output.zip
 tar -cvf output.tar /tmp/test_out
