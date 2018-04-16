997b3fb0d9583a8fc846ac91fc35a24fdab7c271
un <unknown7979@gmail.com>
2018-04-16 18:18:49 -0400

    Generated new .json data files and some .log files not added to the network.

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+
+echo "NOTE: First build the docker container (only do this once for space though) './run_docker build'"
+# Get argument for running the benchmark script.
+ARG1=${1:-run}
+
+if [ $ARG1 == "build" ]
+then
+
 sudo nvidia-docker build -t gpu .
+    
+fi
+
 sudo nvidia-docker run -it \
     --mount type=bind,source="$(pwd)",target=/ML_OD_Benchmarking \
     gpu /bin/bash
